54b8963f98e738bfa57df43f97f01cb5d0ff7e07
SamGhots <sampon251095@gmail.com>
2026-04-20 17:56:09 +0700

    sliderReviews เพิ่ม

---
+++ src/data/CustomerDataReviews.js
@@ -0,0 +1,29 @@
+export const reviewsData = [
+{
+  stars: 5,
+  name: "Sarah M." ,
+  review: "I'm blown away by the quality and style of the clothes I received from Shop.co. From casual wear to elegant dresses, every piece I've bought has exceeded my expectations."
+
+
+},
+{
+  stars: 5,
+  name: "Alex K." ,
+  review: "Finding clothes that align with my personal style used to be a challenge until I discovered Shop.co. The range of options they offer is truly remarkable, catering to a variety of tastes and occasions."
+},
+{
+  stars: 5,
+  name: "James L." ,
+  review: "As someone who's always on the lookout for unique fashion pieces, I'm thrilled to have stumbled upon Shop.co. The selection of clothes is not only diverse but also on-point with the latest trends."
+},
+{
+  stars: 4,
+  name: "Emily R." ,
+  review: "Shop.co has become my go-to destination for fashion. The quality of the clothes is impressive, and I appreciate the attention to detail in their designs. My only wish is that they would expand their size range to be more inclusive."
+},
+{
+  stars: 3,
+  name: "Michael S." ,
+  review: "While I appreciate the variety of clothes available on Shop.co, I found that some items didn't fit as expected. However, their customer service was responsive and helpful in resolving the issue, which I appreciated."
+}
+];
